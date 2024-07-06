b1bff1c70c83b144c952d06e1d0bbf5b446ae922
minhttran <trcongminh.261002@gmail.com>
2024-07-06 20:40:36 +0700

    Admin setup layout mastering

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('admin.dashboard.index');
 });
 
 Route::get('/dashboard', function () {
@@ -17,4 +17,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
